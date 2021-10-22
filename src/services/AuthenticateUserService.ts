010a9f075bf08a3db19b28ecc73565b1ce97a7f1
lucas <lucasgominhaa448@gmail.com>
2021-10-22 18:23:41 -0300

    added changes

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 import { response } from "express";
+import prismaClient from "../prisma";
+import { sign } from "jsonwebtoken"
 
 interface IAcessTokenResponse {
   acess_token: string
@@ -34,6 +36,41 @@ class AuthenticateUserService {
       }
     });
 
+    const { login, id, avatar_url, name } = response.data
+
+    let user = await prismaClient.user.findFirst({
+      where: {
+        github_id: id
+      }
+    });
+
+    if (!user) {
+      await prismaClient.user.create({
+        data: {
+          github_id: id,
+          login,
+          avatar_url,
+          name
+        }
+      }
+      )
+    }
+
+    const token = sign(
+      {
+        user: {
+          name: user.name,
+          avatar_ur: user.avatar_url,
+          id: user.id
+        },
+      },
+      process.env.JWT_SECRET,
+      {
+        subject: user.id,
+        expiresIn: "1d"
+      });
+
+      return { token, user};
 
   }
 }
