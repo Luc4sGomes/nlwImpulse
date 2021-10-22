aa1edc06e519878602087891720fef69f85ef90f
lucas <lucasgominhaa448@gmail.com>
2021-10-22 17:30:55 -0300

    added changes

--- src/controllers/AuthenticateUserController.ts
+++ src/controllers/AuthenticateUserController.ts
@@ -1,7 +1,17 @@
 
+import { Request, Response } from "express";
+import { AuthenticateUserService } from "../services/AuthenticateUserServices";
 
 class AuthenticateUserController {
-  async handle(request, response) {
-  
+  async handle(request: Request, response: Response) {
+
+    const { code } = request.body;
+    
+    const service = new AuthenticateUserService();
+    const result = await service.execute(code);
+
+    return response.json(result);
   }
 }
+
+export { AuthenticateUserController };
--- src/services/AuthenticateUserServices.ts
+++ src/services/AuthenticateUserServices.ts
@@ -1,9 +1,41 @@
+import axios from "axios";
+import { response } from "express";
 
+interface IAcessTokenResponse {
+  acess_token: string
+}
 
-class AuthenticateUserServices {
+interface IUserResponse {
+  avatar_url: string,
+  login: string,
+  id: number,
+  name: string,
+}
+
+class AuthenticateUserService {
   async execute(code: string) {
-    
+
+    const url = "https://github.com/login/oauth/acess_token";
+
+    const { data: acessTokenResponse } = await axios.post<IAcessTokenResponse>(url, null, {
+      params: {
+        client_id: process.env.GITHUB_CLIENT_ID,
+        client_secret: process.env.GITHUB_CLIENT_SECRET,
+        code,
+      },
+      headers: {
+        "Accept": "application/json"
+      }
+    });
+
+    const response = await axios.get<IUserResponse>("https://api.github.com/user", {
+      headers: {
+        authorization: `Bearer ${acessTokenResponse.acess_token}`
+      }
+    });
+
+
   }
 }
 
-export { AuthenticateUserServices };
+export { AuthenticateUserService };
